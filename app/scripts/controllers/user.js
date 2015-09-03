2e506bdec87e478c0f110645a820738ebc5ff19a
david.barkhuizen@gmail.com <david.barkhuizen@gmail.com>
2015-09-03 10:14:46 +0200

    create/edit user - in progress

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,16 +8,69 @@
  * Controller of the mallcmsApp
  */
 angular.module('mallcmsApp')
-  .controller('UserCtrl', function ($scope, $routeParams, DataModel) {
+	.controller('UserCtrl', function ($scope, $routeParams, DataModel) {
 
-  	$scope.dataModel = DataModel;
+		$scope.dataModel = DataModel;
 
-	$scope.dataModel.user = ($routeParams.userId !== 'new')
+		$scope.dataModel.user = ($routeParams.userId !== 'new')
 		
-		? $scope.dataModel.user = $scope.dataModel.users.first(
-			function(x){ 
-  				return (x.id == $routeParams.userId); 
-  			})
+			? $scope.dataModel.user = $scope.dataModel.users.first(
+				function(x){ 
+  					return (x.id == $routeParams.userId); 
+  				})
   		
-  		: new User(null, '', '', '');
-  });	
+  			: new User(null, '', '', '');
+
+    	$scope.originalUserstate = new User(
+      		$scope.dataModel.user.id,
+        	$scope.dataModel.user.name,
+        	$scope.dataModel.user.surname,
+        	$scope.dataModel.user.email
+        	);
+
+    	$scope.password = '';
+    	$scope.confirmPassword = '';
+
+    	$scope.userHasChanged = function() {
+
+    		var changed =
+    			(
+    			($scope.originalUserstate.email !== $scope.dataModel.user.email)
+    			);
+
+    		console.log(changed);
+
+    		return changed;
+    	};
+
+    	$scope.passwordIsValid = function() {
+
+    		var isValid =  
+    			(
+    				($scope.password !== '')
+    				&&
+    				($scope.password.length !== null)
+    				&&
+    				($scope.password.length > 0)
+    				&&
+    				($scope.password === $scope.confirmPassword)
+    			);
+
+    		console.log(isValid);
+    		return isValid;
+    	};
+
+    	$scope.newUserInfoIsCompleteAndValid = function() {
+
+    		var completed =
+    			(
+    				($scope.dataModel.user.name.length > 0)
+    				&&
+    				($scope.dataModel.user.surname.length > 0)
+    				&&
+    				($scope.dataModel.user.email.length > 0)
+	   			);
+
+    		return (completed && $scope.passwordIsValid());
+    	};
+  	});	
